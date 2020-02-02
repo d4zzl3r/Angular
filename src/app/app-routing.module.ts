551e08aec8e0e77d55c300e9452120a03c08008c
D4ZZL3R <nnitinmotwaani002@gmail.com>
2020-02-02 11:18:48 +0530

    Task1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HtmlParser } from '@angular/compiler';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '' ,
+    component : HomeComponent
+  },
+  {
+    path : 'home',
+    component : HomeComponent 
+  },
+  {
+    path : 'contactUs',
+    component : ContactUsComponent
+  },
+  {
+    path : 'aboutus',
+    component : AboutUsComponent 
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { SideNavBarComponent } from './side-nav-bar/side-nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopNavBarComponent,
+    SideNavBarComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
